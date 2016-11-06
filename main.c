f4a4871e08853fe14e62e01d60342ba9ddbbf23d
gwe <guilhem.wetteren@condorcet.be>
2016-11-06 22:20:19 +0100

    Petite modif dans main.c : ajout d'un commentaire

--- main.c
+++ main.c
@@ -7,11 +7,13 @@ int main(){
 	/* 0. Déclaration */
 	int n1, n2, calcul;
 
-	/* 1. Calculs et affichages */
+	/* 1. Calculs */
 	srand((unsigned)time(NULL));
 	n1=hasard(100,200);
 	n2=hasard(100,200);
 	calcul=somme(n1,n2);
+
+	/* 2. Affichages */
 	printf("La somme de %d et de %d vaut %d\n\n", n1, n2, calcul);
 
 	return 0;
