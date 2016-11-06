78950944b9cff58c2007f5ee976620ed34a6baa0
gwe <guilhem.wetteren@condorcet.be>
2016-11-06 22:10:50 +0100

    Les nombres générés sont compris entre 100 et 200

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main(){
 
 	/* 1. Calculs et affichages */
 	srand((unsigned)time(NULL));
-	n1=hasard(10,60);
-	n2=hasard(10,60);
+	n1=hasard(100,200);
+	n2=hasard(100,200);
 	calcul=somme(n1,n2);
 	printf("La somme de %d et de %d vaut %d\n\n", n1, n2, calcul);
 
